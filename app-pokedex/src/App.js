5f1b21e313db13a3edc7866e2ba759f78ad43b1d
StigmaED <elie.dechasse@hotmail.com>
2022-05-16 16:22:24 +0200

    Type/poid/taille

--- app-pokedex/src/App.js
+++ app-pokedex/src/App.js
@@ -13,8 +13,11 @@ const App = () =>
     const toArray = [];
     try
     {
-      const url = "https://pokeapi.co/api/v2/pokemon/${pokemon}"
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
       const res = await axios.get(url)
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name)
+      setPokemonData(toArray);
       console.log(res)
     } 
     catch (e) 
@@ -43,9 +46,36 @@ const App = () =>
           <input type="text" onChange={handleChange} placeholder="Enter Pokemon Name"></input>
         </label>
       </form>
+      {pokemonData.map((data) => 
+      {
+        return (
+          <div className="container">
+            <img />
+            <div className="divTable">
+              <div className="divTableBody"></div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+            </div>
+            <div className="divTableRow">
+                <div className="divTableCell">Height</div>
+                <div className="divTableCell">{" "}{Math.round(data.height * 3.9 )} " </div>
+            </div>
+            <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+            </div>
+            <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+            </div>
+          </div>
+        </div>
 
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
